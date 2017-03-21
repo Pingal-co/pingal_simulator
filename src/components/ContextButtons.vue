f46b549f3b03a76f786fa3447394f79d99b75e71
joshi-rohit <rohit@pingal.co>
2017-03-21 18:40:37 -0400

    Contextual Buttons

---
+++ src/components/ContextButtons.vue
@@ -0,0 +1,27 @@
+<template>
+	<md-card-actions>
+	  <md-button v-for="button, index in buttons" :key="index">
+	  	<div @click="clickAction(button)">{{button.text}}</div>
+	  </md-button>
+	</md-card-actions>
+</template>
+
+<script>
+	export default {
+		props: ['buttons'],
+		computed: {
+			roomChannel() {
+				return this.$store.state.currentRoomChannel
+			}
+		},
+		methods: {
+			clickAction(button) {
+				this.$store.dispatch('pushSlide', {
+		          roomChannel: this.roomChannel,
+		          slide: {text: button.command}, 
+		          event: 'request'
+				})
+			}
+		}
+	}
+</script>
