bc54649b977ec977cfac747fac715b82bfe06399
Laser Nite <lasernite@gmail.com>
2017-05-10 00:35:39 -0400

    styling feedback

--- src/components/FeedbackNav.vue
+++ src/components/FeedbackNav.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="feedback-channel" @click="joinFeedbackChannel">Feedback and Ideas</div>
+	<div :class="['feedback-channel', selected]" @click="joinFeedbackChannel">Feedback and Ideas</div>
 </template>
 
 <script>
@@ -7,7 +7,15 @@
 		computed: {
 			room() {
 				return {id: 1, name: "Feedback and Ideas"}
-			}
+			},
+			// Styling
+			selected() {
+				if ("rooms:" + this.room.id == this.$store.state.currentRoomChannel.topic) {
+					return 'selected'
+				} else {
+					return null
+				}
+			},
 		},
 		methods: {
 			joinFeedbackChannel() {
@@ -19,7 +27,7 @@
 	}
 </script>
 
-<style lang="scss">	
+<style lang="scss" scoped>	
 	.feedback-channel {
 		min-width: 359px;
 		max-width: 359px;
@@ -39,7 +47,6 @@
 		align-items: center;
 		justify-content: center;
 	}
-
 	.feedback-channel:hover {
 		background-color: rgb(244, 128, 45);
 		color: white;
@@ -47,5 +54,10 @@
 	}
 	.feedback-channel:active {
 		background-color: #d9620c;
+	}
+	.selected {
+		background-color: rgb(244, 128, 45);
+		color: white;
+		border-top: 1px solid rgb(244, 128, 45);
 	}
 </style>
